1a1156481899df85cfd513f523db3711a6ab1956
mauropahoor <mauro.pahoor@targetso.com>
2025-10-21 13:38:03 -0300

    First Commit

---
+++ backend/src/main/java/com/zughy/tibia_api/TibiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.zughy.tibia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TibiaApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zughy/tibia_api/TibiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zughy.tibia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
